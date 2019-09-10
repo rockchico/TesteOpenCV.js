5a94dbf0d0d3ea476840eea3e6c80d3691d67c60
Francisco Schwertner <rockchico@gmail.com>
2019-09-10 16:49:44 -0300

    teste inicial

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+import {WW_Wasm} from './util/functions'
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      wasm: {}
+    };
+  }
+  
+  componentDidMount() {
+
+    
+    WW_Wasm().then((result) => {
+      console.log("wasm worker")
+      console.log(result)
+    })
+
+    
+
+
+  }
+  
+  
+
+
+  render() {
+    
+        
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <div></div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
